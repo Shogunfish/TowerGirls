7b2918fba5410aeb6ec167dcc625fddbb557478f
Shogunfish <edgar.j.elliott@gmail.com>
2016-06-16 15:40:37 -0700

    Update MainBody.java

--- MainBody.java
+++ MainBody.java
@@ -10,8 +10,32 @@ Public class Item
 {
 }
 
+public class Preference
+{
+  public String name;
+  public Boolean good;
+}
+
 Public class Character extends Item
 {
+  public int love;
+  public int lust;
+  public int wealth;
+  public int power;
+  
+  public String[3] good;
+  public String [2] bad;
+  
+  public Preference[2] kinks;
+  public Preference turnoff;
+  
+}
+
+Public class Princess1 extends Character 
+{
+  public Item dowry1;
+  public Item dowry2;
+  public Item lustGift;
 }
 
 Public class Power
