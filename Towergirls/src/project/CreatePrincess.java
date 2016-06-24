48c006f3d24966062bcbdc4e5f8f99534cceed05
Jaspirian <jaspirian@gmail.com>
2016-06-24 01:01:07 -0700

    Re-initializing project. Maybe less fucky?

---
+++ Towergirls/src/project/CreatePrincess.java
@@ -0,0 +1,16 @@
+package project;
+
+public class CreatePrincess {
+	public static void main(String[] args) {
+		buildPrincess("Kobold");
+	}
+	
+	public static void buildPrincess (String princess) {
+		
+	}
+	
+	public String readTextFile(String filename, String princess, String info) {
+		return info;
+		
+	}
+}
