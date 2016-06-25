255dc87a97c6fcc0393e5cae77342e0fc56086b1
Jaspirian <jaspirian@gmail.com>
2016-06-25 16:32:25 -0700

    Proof of concept of multiple hover princesses, grabbed by name.
    Generates princess popup window based on setName of JLabels that have
    princess icons.

--- Towergirls/src/testing/HoverTest.java
+++ Towergirls/src/testing/HoverTest.java
@@ -1,10 +1,8 @@
 package testing;
 
-import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.EventQueue;
-import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Image;
 import java.awt.RenderingHints;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -18,13 +16,11 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JWindow;
-import javax.swing.border.EmptyBorder;
-
 import testing.TextFileTesting;
 
 public class HoverTest extends JFrame {
 
-	private JPanel contentPane;
+	JPanel contentPane;
 
 	/**
 	 * Launch the application.
@@ -48,46 +44,87 @@ public class HoverTest extends JFrame {
 	 * Create the frame.
 	 */
 	JWindow window;
-	public HoverTest() {
+	HoverTest() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setLayout(null);
 		setContentPane(contentPane);
 		
-		JLabel Kobold = new JLabel("");
-		contentPane.add(Kobold);
-		Kobold.setLocation(10,10);
-		BufferedImage img = null;
+		//Later, I should have this auto-pull from an array
+		
+		//Kobold JLabel
+				JLabel Kobold = new JLabel("");
+				//Set the name of the JLabel, used for lookup later
+				Kobold.setName("Kobold");
+				contentPane.add(Kobold);
+				Kobold.setLocation(10,10);
+				//Paint the image (half size)
+				paintImage(Kobold);
+				//Add mouse hover listener
+				addHover(Kobold);
+				
+		//Human JLabel
+				JLabel Human = new JLabel("");
+				//Set the name of the JLabel, used for lookup later
+				Human.setName("Human");
+				contentPane.add(Human);
+				Human.setLocation(80,10);
+				//Paint the image (half size)
+				paintImage(Human);
+				//Add mouse hover listener
+				addHover(Human);
+				
+		//Knight JLabel
+				JLabel Knight = new JLabel("");
+				//Set the name of the JLabel, used for lookup later
+				Knight.setName("Knight");
+				contentPane.add(Knight);
+				Knight.setLocation(150,10);
+				//Paint the image (half size)
+				paintImage(Knight);
+				//Add mouse hover listener
+				addHover(Knight);
+		
+	}
+	
+	/*
+	 * Grabs girl from component name and resizes it, then slaps it onto JLabel
+	 */
+	void paintImage(Component comp) {
+		BufferedImage bImg = null;
 		try {
-			img = ImageIO.read(new File("../Girls/Kobold.png"));
+			bImg = ImageIO.read(new File("src/Girls/" + comp.getName() + ".png"));
 		} catch (IOException e) {
+			System.out.println("No file");
 			e.printStackTrace();
 		}
-		BufferedImage resize = new BufferedImage((int)(img.getWidth()*.5),(int)(img.getHeight()*.5),BufferedImage.TYPE_INT_ARGB);
+		BufferedImage resize = new BufferedImage((int)(bImg.getWidth()*.5),(int)(bImg.getHeight()*.5),BufferedImage.TYPE_INT_ARGB);
 		Graphics2D g2d = (Graphics2D)resize.createGraphics();
 		g2d.addRenderingHints(new RenderingHints(RenderingHints.KEY_RENDERING,RenderingHints.VALUE_RENDER_QUALITY));
-		g2d.drawImage(img, 0, 0, (int)(img.getWidth()*.5), (int)(img.getHeight()*.5), null);
+		g2d.drawImage(bImg, 0, 0, (int)(bImg.getWidth()*.5), (int)(bImg.getHeight()*.5), null);
 		
-		Kobold.setSize((int)(img.getWidth()*.5),(int)(img.getHeight()*.5));
+		comp.setSize((int)(bImg.getWidth()*.5),(int)(bImg.getHeight()*.5));
 		ImageIcon icon = new ImageIcon(resize);
-		Kobold.setIcon(icon);
-		
-		Kobold.addMouseListener(new MouseAdapter() {
+		((JLabel) comp).setIcon(icon);
+	}
+	
+	void addHover(Component comp) {
+		comp.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseEntered(MouseEvent me) {
 				window = new JWindow();
 				
 				TextFileTesting test = new TextFileTesting();
 				try {
-					test.readTextFile("src/Text files/Princesses.txt", "Kobold");
+					test.readTextFile("src/Text files/Princesses.txt", comp.getName());
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
 				PrincessTesting princess = new PrincessTesting(test.givePrincess());
 				
 				window.add(princess.provideInput());
-				window.setSize(300,550);
+				window.setSize(300,600);
 				window.setLocation(me.getLocationOnScreen());
 				window.setVisible(true);
 			}
